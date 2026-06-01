c4879c5c744343f50182d64b7cdfbdc66a716e3b
dragonchaser <christian@boltares.de>
2026-06-01 16:56:27 +0200

    allow all servernames for connection
    Signed-off-by: dragonchaser <christian@boltares.de>

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import svgLoader from 'vite-svg-loader'
 export default defineConfig({
   plugins: [vue(), svgLoader()],
   server: {
-    host: true
+    host: true,
+    allowedHosts: true
   }
 })
